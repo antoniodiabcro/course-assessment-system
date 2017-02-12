fb93e836457be1950a1ddb74b15905ffc296fba6
antonio <antoniodiabcro@yahoo.com>
2017-02-12 15:06:07 +0100

    Initiali commit

---
+++ src/main/java/edu/rit/dijkstra/cas/CourseAssessementSystemApplication.java
@@ -0,0 +1,12 @@
+package edu.rit.dijkstra.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAssessementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAssessementSystemApplication.class, args);
+	}
+}
