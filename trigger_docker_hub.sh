91f98652e704de8f78cdab5ad48fb2414512ad03
antonio <antoniodiabcro@yahoo.com>
2017-02-13 01:01:21 +0100

    Changed URL

--- trigger_docker_hub.sh
+++ trigger_docker_hub.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/antoniodiabcro/course-assessment-system/trigger/d807dd61-b052-418c-9d62-b1caaa6a75d6/
+echo "curling to ... https://registry.hub.docker.com/u/antoniodiabcro/course-assessment-system/trigger/8d279df5-f3d0-45a7-83a1-2a09ca0f06e6/"
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/antoniodiabcro/course-assessment-system/trigger/8d279df5-f3d0-45a7-83a1-2a09ca0f06e6/
